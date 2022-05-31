131a696fbdc68873e5c9026d0ee1292407044547
Alexey Loktev <a.a.loktev@yandex.ru>
2022-05-31 14:45:37 +0300

    Выполнено дополнительное задание урока №4

--- script.js
+++ script.js
@@ -1,45 +1,13 @@
-"use strict";
-let lang = prompt("Какой язык предпочитаете?", "ru/en");
-let namePerson = prompt("Введите имя");
+let str = prompt("Введите строку");
 
-function ifElse() {
-  if (lang == "ru") {
-    console.log("Пн, Вт, Ср, Чт, Пт, Сб, Вс");
-  } else if (lang == "en") {
-    console.log("Mn, Tu, Wd, Th, Fr, Sa, Sn");
+function lotOfLetters() {
+  if (!isNaN(str)) {
+    alert("Это не строка!");
+  } else if (str.length > 30) {
+    console.log(str.trim().slice(0, 30) + "...");
+  } else if (str.length < 30) {
+    console.log(str.trim());
   }
 }
 
-function switchCase() {
-  switch (lang) {
-    case "ru":
-      alert("Пн, Вт, Ср, Чт, Пт, Сб, Вс");
-      break;
-    case "en":
-      alert("Mn, Tu, Wd, Th, Fr, Sa, Sn");
-      break;
-  }
-}
-
-function funcArray() {
-  let langArray = [];
-  langArray["ru"] = ["Пн, Вт, Ср, Чт, Пт, Сб, Вс"];
-  langArray["en"] = ["Mn, Tu, Wd, Th, Fr, Sa, Sn"];
-
-  console.log(langArray[lang]);
-}
-
-function rolePerson() {
-  let message =
-    namePerson == "Артём"
-      ? "Директор"
-      : namePerson == "Александр"
-      ? "Преподаватель"
-      : "Студент!";
-  console.log(message);
-}
-
-ifElse();
-switchCase();
-funcArray();
-rolePerson();
+lotOfLetters();
