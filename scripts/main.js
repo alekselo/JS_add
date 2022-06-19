2aec2c222b862a8440a303eff63e36377cf80ebf
Alexey Loktev <a.a.loktev@yandex.ru>
2022-06-20 00:27:16 +0300

    Выполнил задание урока 16

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,18 @@
 "use strict";
-const newSelector = prompt(
-  "Введите название класса (начиная с точки) или ID (начиная с #)"
-);
-const DomElement = function (selector, height, width, bg, fontSize) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
-  this.newElem = function () {
-    let newTag;
-    if (this.selector[0] === ".") {
-      newTag = document.createElement("div");
-      const newClass = this.selector.slice(1);
-      newTag.classList.add(newClass);
-    } else if (this.selector[0] === "#") {
-      newTag = document.createElement("p");
-      const newId = this.selector.slice(1);
-      newTag.setAttribute("id", newId);
-    }
-    newTag.style.height = this.height;
-    newTag.style.width = this.width;
-    newTag.style.background = this.bg;
-    newTag.style.fontSize = this.fontSize;
-    newTag.textContent = newSelector;
-    document.body.prepend(newTag);
-  };
-};
 
-const newContent = new DomElement(newSelector, "100px", "100px", "red", "22px");
-newContent.newElem();
+class First {
+  hello() {
+    console.log("Привет, я метод родителя!");
+  }
+}
+
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log("А я наследуемый метод!");
+  }
+}
+
+let Third = new Second();
+
+Third.hello();
