0184f2987baf36aa7097a3c24689f58650444ede
Alexey Loktev <a.a.loktev@yandex.ru>
2022-06-20 00:13:11 +0300

    рефакторинг кода

--- scripts/main.js
+++ scripts/main.js
@@ -8,31 +8,25 @@ const DomElement = function (selector, height, width, bg, fontSize) {
   this.width = width;
   this.bg = bg;
   this.fontSize = fontSize;
-};
-
-DomElement.prototype.content = function () {
-  if (this.selector[0] === ".") {
-    const newDiv = document.createElement("div");
-    const newClass = this.selector.slice(1);
-    newDiv.classList.add(newClass);
-    newDiv.style.height = this.height;
-    newDiv.style.width = this.width;
-    newDiv.style.background = this.bg;
-    newDiv.style.fontSize = this.fontSize;
-    newDiv.textContent = newSelector;
-    document.body.prepend(newDiv);
-  } else if (this.selector[0] === "#") {
-    const newParagraph = document.createElement("p");
-    const newId = this.selector.slice(1);
-    newParagraph.setAttribute("id", newId);
-    newParagraph.style.height = this.height;
-    newParagraph.style.width = this.width;
-    newParagraph.style.background = this.bg;
-    newParagraph.style.fontSize = this.fontSize;
-    newParagraph.textContent = newSelector;
-    document.body.prepend(newParagraph);
-  }
+  this.newElem = function () {
+    let newTag;
+    if (this.selector[0] === ".") {
+      newTag = document.createElement("div");
+      const newClass = this.selector.slice(1);
+      newTag.classList.add(newClass);
+    } else if (this.selector[0] === "#") {
+      newTag = document.createElement("p");
+      const newId = this.selector.slice(1);
+      newTag.setAttribute("id", newId);
+    }
+    newTag.style.height = this.height;
+    newTag.style.width = this.width;
+    newTag.style.background = this.bg;
+    newTag.style.fontSize = this.fontSize;
+    newTag.textContent = newSelector;
+    document.body.prepend(newTag);
+  };
 };
 
 const newContent = new DomElement(newSelector, "100px", "100px", "red", "22px");
-newContent.content();
+newContent.newElem();
