c0567091f6e7403f3d7618bdf427a79fcb38ac59
Alexey Loktev <a.a.loktev@yandex.ru>
2022-07-05 12:42:35 +0300

    Победил

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 "use strict";
 const getData = () => {
-  return fetch("db.json")
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
+  return fetch("db.json").then((response) => response.json());
 };
 
-const sendData = () => {
+const sendData = (data) => {
   fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "POST",
-    body: JSON.stringify(getData()),
+    body: JSON.stringify(data),
     headers: {
       "Content-type": "application/json; charset=UTF-8",
     },
   })
-    .then((response) => response.json())
+    .then((response) => response.json(data))
     .then((data) => console.log(data))
     .catch((error) => console.log(error));
 };
 
-sendData();
+getData()
+  .then((data) => {
+    console.log(data);
+    sendData(data);
+  })
+  .catch((error) => console.log(error));
